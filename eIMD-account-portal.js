113a5af42cb6caf4bb41a4c02a4612e40bbbb2f8
riotfire <riotfire@gmail.com>
2019-04-05 10:03:12 -0700

    Update eIMD-account-portal.js

--- eIMD-account-portal.js
+++ eIMD-account-portal.js
@@ -1,1 +1,1 @@
-jQuery('#pgc-w5bb657353ce70-1-2').html('<h1>Allegiant Healthcare</h1>').show();
+jQuery('#pgc-w5bb657353ce70-1-2').html('<h1 class="eimd-portal-header"><a href="https://www.einfectionmd.com/allegiant-heath/">Allegiant Healthcare</a></h1>').show();
