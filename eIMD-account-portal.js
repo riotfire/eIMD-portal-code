9052288ff29220d2d3a013890d546e67169a60f3
riotfire <riotfire@gmail.com>
2019-05-01 11:02:05 -0700

    Update eIMD-account-portal.js

--- eIMD-account-portal.js
+++ eIMD-account-portal.js
@@ -1,5 +1,5 @@
 // Hiding the main navigation of the iHealthSpot theme
-jQuery('#pgc-w5bb657353ce70-1-2').html('<h1 class="eimd-portal-header"><a href="https://www.einfectionmd.com/allegiant-heath/">Allegiant Healthcare</a></h1>').show();
+jQuery('#pgc-w5bb657353ce70-1-2').html('<h1 class="eimd-portal-header"><a href="https://www.einfectionmd.com/allegianthealth/">Allegiant Healthcare</a></h1>').show();
 
 // Function to prep the form data before sending
 jQuery.fn.serializeObject = function()
