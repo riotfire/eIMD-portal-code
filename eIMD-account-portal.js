9f9c14c75a95f66145c4cfc3bcf91592546dfb02
riotfire <riotfire@gmail.com>
2019-05-30 16:55:44 -0700

    updated to include multi audit tools and logs. Added PPE and HW html

--- eIMD-account-portal.js
+++ eIMD-account-portal.js
@@ -1,6 +1,9 @@
 // Hiding the main navigation of the iHealthSpot theme
 jQuery('#pgc-w5bb657353ce70-1-2').html('<h1 class="eimd-portal-header"><a href="https://www.einfectionmd.com/allegianthealth/">Allegiant Healthcare</a></h1>').show();
 
+
+
+
 // Function to prep the form data before sending
 jQuery.fn.serializeObject = function()
 {
@@ -19,27 +22,33 @@ jQuery.fn.serializeObject = function()
    return o;
 };
 
-
-// Hand Washing Form 
-var eIMDForm = jQuery('#hwlog'),
-    eIMDHwForm = 'https://script.google.com/macros/s/AKfycbzvXdTpzL-ZrEN1oGg-cTJflKCNoe-Q1t-wKjePNE_Kka6TQEgB/exec',
-    eIMDSubmitButton = jQuery('#submit-form'),
-    eIMDSubmitButtonLabel = jQuery('#submit-form').html(),
-    eIMDSubmitButtonLoading = "Please Wait...";
-
-jQuery(eIMDSubmitButton).on('click', function(e) {
-  e.preventDefault(); // Don't refresh the page on click
-  
-  eIMDSubmitButton.html(eIMDSubmitButtonLoading); // Change the button label to indicate the form is being submitted
-
+jQuery.fn.eimdPushLog = function(form, url, button, buttonLabel){
   var jqxhr = jQuery.ajax({
-    url: eIMDHwForm,
+    url: url,
     method: "GET",
     dataType: "json",
-    data: eIMDForm.serializeObject(),
-  }).success( function(){ 
+    data: form.serializeObject(),
+  }).success( function(e){
       alert('Saved Record'); // Tell the user the form has submitted correctly.
-      eIMDForm.trigger('reset'); // Reset the form
-      eIMDSubmitButton.html(eIMDSubmitButtonLabel); // Reset the button label to the original value to indicate it is ready for another save
+      form.trigger('reset'); // Reset the form
+      button.html(buttonLabel); // Reset the button label to the original value to indicate it is ready for another save
+      console.log(e);
     });
+}
+
+
+// Hand Washing Form
+var eimdAuditForm = jQuery('#eimdAudit'),
+    eimdUrl = jQuery('#eimdLogURL').val(),
+    eimdSubmitButton = jQuery('#submit-form'),
+    eimdSubmitButtonLabel = jQuery('#submit-form').html(),
+    eimdSubmitButtonLoading = "Please Wait...";
+
+jQuery(eimdSubmitButton).on('click', function(e) {
+  e.preventDefault(); // Don't refresh the page on click
+  eimdSubmitButton.html(eimdSubmitButtonLoading); // Change the button label to indicate the form is being submitted
+
+  // Push form data to log
+  eimdAuditForm.eimdPushLog(eimdAuditForm,eimdUrl,eimdSubmitButton,eimdSubmitButtonLabel);
+
 });
